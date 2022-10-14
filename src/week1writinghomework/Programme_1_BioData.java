d24d245a05a9ad975af5efde6e1b693ee1a743b9
Nareshgothadiya <naresh2u@gmail.com>
2022-10-14 13:11:42 +0100

    java-week-1-writing-hw-naresh

---
+++ src/week1writinghomework/Programme_1_BioData.java
@@ -0,0 +1,24 @@
+package week1writinghomework;
+
+ //1. Write a program that prints your BIO Data.
+
+public class Programme_1_BioData {
+
+    public static void main(String[] argh) {
+        System.out.println("              BIO DATA                ");
+        System.out.println("Name              : Naresh Gothadiya");
+        System.out.println("Mobile            : 07889 142758");
+        System.out.println("Email             : nareshg@gmail.com");
+        System.out.println("Gender            : Male");
+        System.out.println("Date of Birth     : 13.05.1982");
+        System.out.println("Marital Status    : Married");
+        System.out.println("Languages Known   : English, Gujarati and Hindi");
+        System.out.println("Qualification     : Software Tester");
+        System.out.println("Experience        : Agile Tester with 5 years of experience");
+        System.out.println("Address           : 123 California Street, London AB1 2CD");
+        System.out.println("Place             : London");
+        System.out.println("Date              : 13.10.2022");
+        System.out.println("Signature         : GNaresh");
+    }
+}
+
---
+++ src/week1writinghomework/Programme_4_RightTriangle.java
@@ -0,0 +1,24 @@
+package week1writinghomework;
+
+/**
+ * 4. Write a Java Program to print below triangle
+ * <p>
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+
+public class Programme_4_RightTriangle {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
